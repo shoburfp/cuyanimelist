460474cd9cc5a18c6e05c503ca431545d8bfdf84
shoburfp <shoburferysonp@gmail.com>
2023-10-23 11:44:31 +0800

    update search

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -5,10 +5,12 @@ const AnimeList = ({ api }) => {
     return (
 
         <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-            {api.data.map((anime) => {
+            {api.data.map((anime, index) => {
                 return (
                     <Link href={`/${anime.mal_id}`} className="cursor-pointer text-white
-                     hover:text-red-600 transition-all">
+                     hover:text-red-600 transition-all"
+                    key={index}
+                    >
                         <Image
                             src={anime.images.webp.image_url}
                             alt="..."
--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -8,15 +8,14 @@ const InputSearch = () => {
     const router = useRouter()
 
     const handleSearch = (event) => {
+        const keyword = searchRef.current.value
+
+        if(!keyword) return
+
         if (event.key === "Enter" || event.type === "click") {
             event.preventDefault()
-            const keyword = searchRef.current.value
-
             router.push(`/search/${keyword}`)
-        } else {
-            event.preventDefault()
-            router.push('/')
-        }
+        } 
     }
 
     return (
